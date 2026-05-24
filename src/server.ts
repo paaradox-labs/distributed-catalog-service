05ad6063590f5489a92dda1530b5cea7fbb924cb
Mr. Paradox <avyas8927@gmail.com>
2026-05-24 17:26:07 +0530

    check

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import logger from "./config/logger";
 
 const startServer = () => {
     const PORT = 5502;
+
     try {
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
