933c00ec5c573de120bdd57153175d1710fe8719
Mr. Paradox <avyas8927@gmail.com>
2026-05-24 17:25:11 +0530

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        ignores: [
+            "dist",
+            "node_modules",
+            "eslint.config.mjs",
+            "package-lock.json",
+            "*.spec.ts",
+            "test",
+        ],
+    },
+    {
+        rules: {
+            "no-console": "warn",
+            "dot-notation": "error",
+        },
+    },
+);
