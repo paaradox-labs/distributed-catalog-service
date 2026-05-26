c7a35e57447e3ba1a7febcf122a409ad6aad5837
Mr. Paradox <avyas8927@gmail.com>
2026-05-26 19:33:01 +0530

    add: create product endpoint

---
+++ src/product/product-router.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { asyncWrapper } from "../common/utils/wrapper";
+import authenticate from "../common/middlewares/authenticate";
+import { canAccess } from "../common/middlewares/canAccess";
+import { Roles } from "../common/constants";
+import { ProductController } from "./product-controller";
+import createProductValidator from "./create-product-validator";
+import { ProductService } from "./product-service";
+import fileupload from "express-fileupload";
+
+const router = express.Router();
+
+const productService = new ProductService();
+const productController = new ProductController(productService);
+
+router.post(
+    "/",
+    authenticate,
+    canAccess([Roles.ADMIN, Roles.MANAGER]),
+    fileupload(),
+    createProductValidator,
+    asyncWrapper(productController.create),
+);
+
+export default router;
