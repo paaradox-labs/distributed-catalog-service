0d9700ade06a75fbb2ccda70a2a2caf09fdeaa87
Mr. Paradox <avyas8927@gmail.com>
2026-05-27 15:46:27 +0530

    add: update product with image update

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -60,4 +60,60 @@ export class ProductController {
             id: newProduct._id,
         });
     };
+
+    update = async (req: Request, res: Response, next: NextFunction) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return next(createHttpError(400, result.array()[0].msg as string));
+        }
+
+        const { productId } = req.params;
+
+        let imageName: string | undefined;
+        let oldImage: string | undefined;
+
+        if (req.files?.image) {
+            oldImage = await this.productService.getProductImage(
+                productId as string,
+            );
+
+            const image = req.files.image as UploadedFile;
+            imageName = uuidv4();
+
+            await this.storage.upload({
+                filename: imageName,
+                fileData: image.data,
+            });
+
+            await this.storage.delete(oldImage!);
+        }
+
+        const {
+            name,
+            description,
+            priceConfiguration,
+            attributes,
+            tenantId,
+            categoryId,
+            isPublish,
+        } = req.body;
+
+        const product = {
+            name,
+            description,
+            priceConfiguration: JSON.parse(priceConfiguration as string),
+            attributes: JSON.parse(attributes as string),
+            tenantId,
+            categoryId,
+            isPublish,
+            // tood: image upload
+            image: imageName ? imageName : (oldImage as string),
+        };
+
+        await this.productService.updateProduct(productId as string, product);
+
+        res.json({
+            id: productId,
+        });
+    };
 }
