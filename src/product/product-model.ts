6ae2b8e12ca15540751859d5a5350a7b588a8632
Mr. Paradox <avyas8927@gmail.com>
2026-05-28 14:20:19 +0530

    add: product pagination

--- src/product/product-model.ts
+++ src/product/product-model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const attributeValueScehema = new mongoose.Schema({
     name: {
@@ -57,5 +58,7 @@ const productSchema = new mongoose.Schema(
         timestamps: true,
     },
 );
+
+productSchema.plugin(aggregatePaginate);
 
 export default mongoose.model("Product", productSchema);
