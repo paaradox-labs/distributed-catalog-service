301921901c757d3a2d84bf36c4efa8f20197eab4
Mr. Paradox <avyas8927@gmail.com>
2026-07-18 14:35:54 +0530

    implement sasl

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,14 +1,30 @@
-import { Kafka, Producer } from "kafkajs";
+import { Kafka, KafkaConfig, Producer } from "kafkajs";
 import { MessageProducerBroker } from "../common/types/broker";
+import config from "config";
 
 export class KafkaProducerBroker implements MessageProducerBroker {
     private producer: Producer;
 
     constructor(clientId: string, brokers: string[]) {
-        const kafka = new Kafka({
+        let kafkaConfig: KafkaConfig = {
             clientId,
             brokers,
-        });
+        };
+
+        if (process.env.NODE_ENV === "production") {
+            kafkaConfig = {
+                ...kafkaConfig,
+                ssl: true,
+                connectionTimeout: 450000,
+                sasl: {
+                    mechanism: "plain",
+                    username: config.get("kafka.sasl.username"),
+                    password: config.get("kafka.sasl.password"),
+                },
+            };
+        }
+
+        const kafka = new Kafka(kafkaConfig);
         this.producer = kafka.producer();
     }
 
